9e826b6666fdf681141dc59f8eb80cac3e2d5ff3
Luca Terrazzan <luca.terraz@gmail.com>
2020-01-31 16:58:53 +0100

    added views :bomb:

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import { Jira } from './jira';
 import express from 'express';
+import path from 'path';
+import ejs from 'ejs';
 
 const app = express();
 const jira = new Jira();
+
+app.set('views', path.join(__dirname, '../../views'));
+app.engine('html', ejs.renderFile);
+app.set('view engine', 'html');
 
 app.get('/', async (req, res) => {
     try {
@@ -12,6 +18,11 @@ app.get('/', async (req, res) => {
     } catch (e) {
       console.error('error:', e);
     }
+  }
+);
+
+app.get('/board', (req, res) => {
+    res.render('index')
   }
 );
 
